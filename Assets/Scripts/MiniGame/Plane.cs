62eeba4531fef2a622274906e52636d022ce2dfe
윤동영 <ehddud9608@naver.com>
2025-02-18 19:05:06 +0900

    [Feat] 미니게임 씬 구상 및 캐릭터 애니메이션 기능 추가
    미니게임 씬 구상 및 캐릭터 애니메이션 기능 추가

---
+++ Assets/Scripts/MiniGame/Plane.cs
@@ -0,0 +1,83 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    Animator animator = null;
+    Rigidbody2D _rigidbody = null;
+
+    public float flapForce = 6f;
+    public float forwardSpeed = 3f;
+    public bool isDead = false;
+    float deathCoolDown = 0f;
+
+    bool isFlap = false;
+
+    public bool GodMode = false;
+
+    private void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+        _rigidbody = transform.GetComponent<Rigidbody2D>();
+
+        if(animator == null)
+        {
+            Debug.LogError("Not Founded Animator");
+        }
+        if(_rigidbody == null)
+        {
+            Debug.LogError("Not Founded RigidBody2D");
+        }
+    }
+    private void Update()
+    {
+        if(isDead)
+        {
+            if(deathCoolDown<=0)
+            {
+                if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+                {
+                    //게임 재시작 함수 호출
+                }
+            }
+            else
+            {
+                deathCoolDown -= Time.deltaTime; 
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                isFlap = true;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+
+        Vector3 velocity = _rigidbody.velocity;
+        velocity.x = forwardSpeed;
+        
+        if(isFlap)
+        {
+            _rigidbody.velocity = velocity;
+
+            float angle = Mathf.Clamp((_rigidbody.velocity.y * 10f), -90, 90); // y축에 있는 중력의 속도는 -90에서 90f 사이로 정의
+            float lerpAngle = Mathf.Lerp(transform.rotation.eulerAngles.z, angle, Time.fixedDeltaTime * 5f);
+            transform.rotation = Quaternion.Euler(0, 0, lerpAngle);
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (GodMode) return;
+        if (isDead) return;
+
+        animator.SetInteger("IsDie", 1);
+        isDead = true;
+        deathCoolDown = 1f;
+    }
+}
