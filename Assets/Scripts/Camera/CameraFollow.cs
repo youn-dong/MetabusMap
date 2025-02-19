735c9231e1b56ace245cb6247332c6b7d14aa22c
윤동영 <ehddud9608@naver.com>
2025-02-19 09:10:42 +0900

    [Feat] 타일맵 외곽 카메라 이동제한
    타일맵 외곽 카메라 이동제한

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,7 +6,12 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float cameraSpeed = 0.1f;
+    private Camera maincamera;
 
+    protected void Start()
+    {
+        maincamera = Camera.main;
+    }
     public void SetCamera(Transform target) //카메라가 따라다닐 객체
     {
         target = player;
@@ -21,6 +26,22 @@ public class CameraFollow : MonoBehaviour
 
             transform.position = Vector3.Lerp(transform.position, playerPosition, cameraSpeed); //애니메이션의 자연스러움을 연출하기 위해 
                                                                                                 // Lerp함수를 적용
+        }
+        if (player.transform.position.x < -8f)
+        {
+            Vector3 cameraPosition = new Vector3(-8f, player.position.y, 0);
+        }
+        else if (player.transform.position.x > 30f)
+        {
+            Vector3 cameraPosition = new Vector3(30f, player.position.y, 0);
+        }
+        if (player.transform.position.y < -2.6f)
+        {
+            Vector3 cameraPosition = new Vector3(player.position.x, -2f, 0);
+        }
+        else if (player.transform.position.y > 29f)
+        {
+            Vector3 cameraPosition = new Vector3(player.position.x, 29.5f, 0);
         }
     }
 }
